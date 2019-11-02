b102bb7e21fc0e20d8ab917b1195501a972ee974
rzdhop <45741890+rzdhop@users.noreply.github.com>
2019-11-02 14:33:43 +0100

    Update 2input-mononeurone.py

--- 2input-mononeurone.py
+++ 2input-mononeurone.py
@@ -14,7 +14,7 @@ class neurone():
     def guess(self):
         self.actualOutput = self.activationFunction(np.dot(self.input.T, self.weight.T))
     def activationFunction(self, output):
-        return tanh(output)
+        return output #tanh() for tangeante hyperbolique
     def Improve(self):
         for weight in self.weight:
             for input in self.input:
@@ -23,18 +23,11 @@ class neurone():
 
 if __name__ == "__main__":
     firstInput = np.array([1.35435, 1.333333])
-    goal = 0.497182570654321
+    goal = 999
     myNeurone = neurone(inputs=firstInput)
-    for training in range(10000):
+    for training in range(100):
         myNeurone.guess()
         myNeurone.error = goal - myNeurone.actualOutput
         myNeurone.Improve()
         
-    print(f"\n--[{myNeurone.actualOutput}, {myNeurone.weight}, {myNeurone.error}]--\n")
-
-    print("final output :", myNeurone.actualOutput, chr(0xa), "final weight :", myNeurone.weight)
-
-
-
-
-
+    print(chr(0xa), "final output :", myNeurone.actualOutput, chr(0xa), "final weight :", myNeurone.weight, chr(0xa), "final error :", myNeurone.error, chr(0xa))
