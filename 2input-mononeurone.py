11d53fcc4bffbabd6889e0df59a00f2b3f155664
rzdhop <45741890+rzdhop@users.noreply.github.com>
2019-11-03 19:14:05 +0100

    Added the bies to avoid input "0" error

--- 2input-mononeurone.py
+++ 2input-mononeurone.py
@@ -5,12 +5,12 @@ from math import tanh
 
 class neurone():
     def __init__(self, inputs):
-        self.input = inputs
-        self.weight = 2 * np.random.random((1, 2)) - 1
         self.actualOutput = 0
         self.error = 0
         self.learningRate = 0.1
-
+        self.biesValue = np.array([1])
+        self.input = np.append(inputs, self.biesValue)
+        self.weight = 2 * np.random.random((1, 3)) - 1
     def guess(self):
         self.actualOutput = self.activationFunction(np.dot(self.input.T, self.weight.T))
     def activationFunction(self, output):
@@ -19,13 +19,13 @@ class neurone():
         for weight in self.weight:
             for input in self.input:
                 weight += self.error * input * self.learningRate
-                print(weight, "-----", self.actualOutput)
+                print(self.weight, "-----", self.actualOutput)
 
 if __name__ == "__main__":
-    firstInput = np.array([1.35435, 1.333333])
+    firstInput = np.array([0, 0])
     goal = 999
     myNeurone = neurone(inputs=firstInput)
-    for training in range(100):
+    for training in range(1000):
         myNeurone.guess()
         myNeurone.error = goal - myNeurone.actualOutput
         myNeurone.Improve()
